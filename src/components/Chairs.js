e82417e6ede576aaf477c3a82606fc9c219f31c2
jubrealguy <whizkidjubrealguy@gmail.com>
2024-07-13 21:28:55 +0100

    API

--- src/components/Chairs.js
+++ src/components/Chairs.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import {chairs} from "../data/data"
+import React, { useState, useEffect } from 'react';
 import add_cart from "../img/add_cart.png"
 
 const Chairs = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = 'https://timbu-get-all-products.reavdev.workers.dev/?organization_id=9dc83d54958d4e28a313c2bd154b1379&reverse_sort=false&page=1&size=10&Appid=UBB2UMUP2FSHXH1&Apikey=0a067da6325d42b996d4c5be95eb034a20240713164907481886'
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result.items);
+        console.log(result.items)
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
     return (
     <React.Fragment>
         <div className="section__chairs">
-            {chairs.map((chair, index) => {
+            {data.map((chair) => {
+                const imageUrl = chair.photos && chair.photos.length > 0 ? `https://api.timbu.cloud/images/${chair.photos[0].url}` : null;
                 return (
-                    <div key={index} className="card">
-                        <img src={chair.img} alt="" className="card-img" />
-                        <p className="card-title">{chair.title}</p>
+                    <div key={chair.unique_id} className="card">
+                        <img src={imageUrl} alt="" className="card-img" />
+                        <p className="card-title">{chair.name}</p>
                         <div className="card-price">
                             <p>
-                                <span className="card-price-1">${chair.price1}</span>
+                                <span className="card-price-1">${chair.current_price[0].USD[0]}</span>
                                 <span className="card-price-2">${chair.price2}</span>
                             </p>
                             <img src={add_cart} alt="" className="card-carting"/>
