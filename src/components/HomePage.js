6f1920b6b3001af9ceb7cff6966ed283f3525554
jubrealguy <whizkidjubrealguy@gmail.com>
2024-07-14 19:37:57 +0100

    pagination

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Chairs from "./Chairs";
 import SectionNav from "./SectionNav";
 import { Route, Routes } from 'react-router-dom';
@@ -6,12 +6,23 @@ import Bed from "./Bed";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
-    const totalPages = Math.ceil(30 / 10);
+    // const totalPages = Math.ceil(30 / 10);
+    const [activeIndex, setActiveIndex] = useState(0);
 
-    const paginationItems = [];
-    for (let i = 1; i <= totalPages; i++) {
-        paginationItems.push(<li key={i} className={`section__pages-${i}`}><Link to={`/?page=${i}`}>{i}</Link></li>);
-    }
+    const handleIndexClick = (index) => {
+        setActiveIndex(index);
+    };
+
+    // const paginationItems_ = [];
+    // for (let i = 1; i <= totalPages; i++) {
+    //     paginationItems.push(<li key={i} onClick={() => setClas(i)} className={`section__pages-${clas}`}><Link to={`/?page=${i}`}>{i}</Link></li>);
+    // }
+
+    const paginationItems = <ul className="section__pages">
+                                <li key={1} className={ activeIndex === 0 ?`section__pages-1`: ''} onClick={() => handleIndexClick(0)}><Link to={`/?page=1`}>1</Link></li>
+                                <li key={2} className={ activeIndex === 1 ?`section__pages-1`: ''} onClick={() => handleIndexClick(1)}><Link to={`/?page=2`}>2</Link></li>
+                                <li key={3} className={ activeIndex === 2 ?`section__pages-1`: ''} onClick={() => handleIndexClick(2)}><Link to={`/?page=3`}>3</Link></li>
+                            </ul>
 
     return (
         <div>
@@ -22,10 +33,7 @@ const HomePage = () => {
                     <Route path="/" element={<Chairs page={1} />} />
                     <Route path="/bed" element={<Bed />} />
                 </Routes>
-    
-                <ul className="section__pages">
-                    {paginationItems}
-                </ul>
+                {paginationItems}
             </section>
         </div>
     );
