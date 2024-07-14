bc898a2c1a5bbe0ec958f538c90b39291b470ec0
jubrealguy <whizkidjubrealguy@gmail.com>
2024-07-14 18:59:56 +0100

    pagination

--- src/components/Chairs.js
+++ src/components/Chairs.js
@@ -1,75 +1,79 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import add_cart from "../img/add_cart.png";
 import NumContext from './NumContext';
 
 const Chairs = () => {
-  const { num, setNum, addItemToCart } = useContext(NumContext);
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const { num, setNum, addItemToCart } = useContext(NumContext);
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const page = parseInt(searchParams.get('page')) || 1;
 
-  const addCart = (e, chair) => {
-    e.stopPropagation();
-    setNum(num + 1);
-    addItemToCart(chair);
-  };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const url = `https://timbu-get-all-products.reavdev.workers.dev/?organization_id=9dc83d54958d4e28a313c2bd154b1379&reverse_sort=false&page=${page}&size=10&Appid=UBB2UMUP2FSHXH1&Apikey=0a067da6325d42b996d4c5be95eb034a20240713164907481886`;
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const result = await response.json();
+                setData(result.items);
+                setLoading(false);
+            } catch (error) {
+                setError(error);
+                setLoading(false);
+            }
+        };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const url = 'https://timbu-get-all-products.reavdev.workers.dev/?organization_id=9dc83d54958d4e28a313c2bd154b1379&reverse_sort=false&page=1&size=10&Appid=UBB2UMUP2FSHXH1&Apikey=0a067da6325d42b996d4c5be95eb034a20240713164907481886';
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const result = await response.json();
-        setData(result.items);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-        setLoading(false);
-      }
+        fetchData();
+    }, [page]);
+
+    const addCart = (e, chair) => {
+        e.stopPropagation();
+        setNum(num + 1);
+        addItemToCart(chair);
     };
 
-    fetchData();
-  }, []);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
-  return (
-    <React.Fragment>
-      <div className="section__chairs">
-        {data.map((chair) => {
-          const imageUrl = chair.photos && chair.photos.length > 0 ? `https://api.timbu.cloud/images/${chair.photos[0].url}` : null;
-          return (
-            <div key={chair.unique_id} className="card">
-              <Link 
-                to="/product" 
-                state={{chair}}
-                className="card-link">
-                <img src={imageUrl} alt="" className="card-img" />
-                <p className="card-title">{chair.name}</p>
-                <div className="card-price">
-                  <p>
-                    <span className="card-price-1">${chair.current_price[0].USD[1]}</span>
-                    <span className="card-price-2">${chair.current_price[0].USD[0]}</span>
-                  </p>
-                </div>
-              </Link>
-              <img 
-                src={add_cart} 
-                alt="" 
-                className="card-carting" 
-                onClick={(e) => addCart(e, chair)} 
-              />
+    return (
+        <React.Fragment>
+            <div className="section__chairs">
+                {data.map((chair) => {
+                    const imageUrl = chair.photos && chair.photos.length > 0 ? `https://api.timbu.cloud/images/${chair.photos[0].url}` : null;
+                    return (
+                        <div key={chair.unique_id} className="card">
+                            <Link 
+                                to="/product" 
+                                state={{ chair }}
+                                className="card-link"
+                            >
+                                <img src={imageUrl} alt="" className="card-img" />
+                                <p className="card-title">{chair.name}</p>
+                                <div className="card-price">
+                                    <p>
+                                        <span className="card-price-1">${chair.current_price[0].USD[1]}</span>
+                                        <span className="card-price-2">${chair.current_price[0].USD[0]}</span>
+                                    </p>
+                                </div>
+                            </Link>
+                            <img 
+                                src={add_cart} 
+                                alt="" 
+                                className="card-carting" 
+                                onClick={(e) => addCart(e, chair)} 
+                            />
+                        </div>
+                    );
+                })}
             </div>
-          )
-        })}
-      </div>
-    </React.Fragment>
-  );
+        </React.Fragment>
+    );
 }
 
 export default Chairs;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,29 +1,36 @@
-import { React } from "react";
+import React from "react";
 import Chairs from "./Chairs";
-import SectionNav from "./SectionNav"
+import SectionNav from "./SectionNav";
 import { Route, Routes } from 'react-router-dom';
-import Bed from "./Bed"
+import Bed from "./Bed";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
+    // Assuming 30 products and 10 products per page
+    const totalPages = Math.ceil(30 / 10); // Calculate total pages
+
+    // Generate pagination items
+    const paginationItems = [];
+    for (let i = 1; i <= totalPages; i++) {
+        paginationItems.push(<li key={i} className={`section__pages-${i}`}><Link to={`/?page=${i}`}>{i}</Link></li>);
+    }
+
     return (
         <div>
             <h3 className="header__text">Welcome to our furniture collection, where style meets comfort and quality</h3>
             <section className="section">
                 <SectionNav />
                 <Routes>
-                    <Route path="" element={<Chairs />} />
-                    <Route path="bed" element={<Bed />} />
+                    <Route path="/" element={<Chairs page={1} />} />
+                    <Route path="/bed" element={<Bed />} />
                 </Routes>
+                {/* Pagination */}
                 <ul className="section__pages">
-                    <li className="section__pages-1">1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>5</li>
+                    {paginationItems}
                 </ul>
             </section>
         </div>
-    )
+    );
 }
 
-export default HomePage
+export default HomePage;
